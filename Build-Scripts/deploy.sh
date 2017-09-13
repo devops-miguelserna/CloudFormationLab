45340b86f9de4f7f37db982b3c93c3d85134e84b
Carlos Lopez <liche60@users.noreply.github.com>
2017-09-13 11:08:18 -0500

    fix to deploy script

--- Build-Scripts/deploy.sh
+++ Build-Scripts/deploy.sh
@@ -1,6 +1,7 @@
 #aws cloudformation create-stack --stack-name test$(date +%s) --template-body file:////Users/ARRANZM/Documents/CursoAWS/cloudformation/partes/nested.template --region us-west-1
 
-
-
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PROJECT_HOME="${SCRIPTS_DIR}/.."
+echo $PROJECT_HOME
 name=test-Stack-Jenkins
 aws cloudformation update-stack --stack-name $name --template-body file:////Users/ARRANZM/Documents/CursoAWS/cloudformation/partes/nested.template --region eu-central-1 
